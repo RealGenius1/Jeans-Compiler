e934439ee707c8fc43e55330c02dbcfefe65bb66
RealGenius1 <sptho24@gmail.com>
2025-12-11 07:17:21 -0500

    fixed errors

--- src/jeanscompiler/JavaGenerator.java
+++ src/jeanscompiler/JavaGenerator.java
@@ -22,63 +22,52 @@ public class JavaGenerator {
 
             if (n instanceof JortDecl vd) {
                 out.append("    int ").append(vd.name).append(" = " + vd.val + ";\n");
-            }
-
-            else if (n instanceof Assignment as) {
+            } else if (n instanceof Assignment as) {
                 out.append("    ").append(as.name).append(" = ").append(as.value).append(";\n");
-            }
-
-            else if (n instanceof Print p) {
+            } else if (n instanceof Print p) {
                 out.append("    System.out.println(").append(p.value).append(");\n");
-            }
-            else if (n instanceof strDecl sd){
+            } else if (n instanceof strDecl sd) {
                 out.append("    String ").append(sd.name).append(" = \"" + sd.val + "\";\n");
-            }
-            else if (n instanceof vestDecl vd){
+            } else if (n instanceof vestDecl vd) {
                 out.append("    char ").append(vd.name).append(" = '" + vd.val + "';\n");
-            }
-            else if (n instanceof JeggingDecl jd){
+            } else if (n instanceof JeggingDecl jd) {
                 out.append("    double ").append(jd.name).append(" = " + jd.val + ";\n");
-            }
-            else if (n instanceof hatDuckl vd){
+            } else if (n instanceof hatDuckl vd) {
                 out.append("    boolean ").append(vd.name).append(" = " + vd.val + ";\n");
-            }
-            else if (n instanceof zipUp zu){
+            } else if (n instanceof zipUp zu) {
                 out.append("     " + zu.name + "++;\n");
-            }
-            else if (n instanceof zipDown zd) {
+            } else if (n instanceof zipDown zd) {
                 out.append("     " + zd.name + "--;\n");
-            }
-            else if (n instanceof washDuckl wd){
+            } else if (n instanceof washDuckl wd) {
                 out.append("    System.gc();\n");
-            
-        }
 
-        out.append("  }\n}\n");
+            }
+        }
+            out.append("  }\n}\n");
 //        File file = new File("Jeans.java");
-        FileWriter writer = new FileWriter("C:\\Users\\sptho\\Downloads\\Github\\Jeans-Compiler\\src\\jeanscompiler\\Jeans.java");
-        writer.write(out.toString());
-        writer.close();
+            FileWriter writer = new FileWriter("C:\\Users\\sptho\\Downloads\\Github\\Jeans-Compiler\\src\\jeanscompiler\\Jeans.java");
+            writer.write(out.toString());
+            writer.close();
 
-        String srcPath = "src/jeanscompiler/Jeans.java";
+            String srcPath = "src/jeanscompiler/Jeans.java";
 
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        int result = compiler.run(null, null, null, srcPath);
+            JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+            int result = compiler.run(null, null, null, srcPath);
 
-        if (result != 0) {
-            System.out.println("Compilation failed");
+            if (result != 0) {
+                System.out.println("Compilation failed");
+            }
+
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-cp", "src",
+                    "jeanscompiler.Jeans"
+            );
+
+            pb.inheritIO();
+            Process p = pb.start();
+            p.waitFor();
+
+            return out.toString();
         }
-
-        ProcessBuilder pb = new ProcessBuilder(
-                "java",
-                "-cp", "src",
-                "jeanscompiler.Jeans"
-        );
-
-        pb.inheritIO();
-        Process p = pb.start();
-        p.waitFor();
-
-        return out.toString();
     }
-}
